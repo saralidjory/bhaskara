9d29306284201650e6c7a7b0a554e207e35d3e79
saralidjory <38309360+saralidjory@users.noreply.github.com>
2018-04-12 02:52:56 -0300

    Add files via upload

---
+++ bhaskara.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace baskara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, delta, x1, x2;
+
+            Console.WriteLine("Digite o valor de a, b e c, nesta ordem, separados por espaço");
+
+            string[] vet = Console.ReadLine().Split(' ');
+
+            a = double.Parse(vet[0], CultureInfo.InvariantCulture);
+            b = double.Parse(vet[1], CultureInfo.InvariantCulture);
+            c = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            delta = b * b - 4 * a * c;
+
+            if (a == 0.0 || delta < 0.0)
+            {
+                Console.WriteLine("IMPOSSÍVEL CALCULAR: Delta não pode ser negativo e o coeficiente não pode ser nulo.");
+            }
+            else
+            {
+                x1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
+                x2 = (-b - Math.Sqrt(delta)) / (2.0 * a);
+                Console.WriteLine("X1 = " + x1.ToString("F4", CultureInfo.InvariantCulture)) ;
+                Console.WriteLine("X2 = " + x2.ToString("F4", CultureInfo.InvariantCulture));
+            }
+            Console.ReadLine();
+        }
+    }
+}
